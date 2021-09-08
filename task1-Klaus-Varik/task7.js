01ad70e1488d4986b64f170522bde2205c058997
Klaus Varik <klaus.varik@vikk.ee>
2021-09-08 14:42:56 +0300

    task7 done nüüd kindlasti

--- task1-Klaus-Varik/task7.js
+++ task1-Klaus-Varik/task7.js
@@ -10,13 +10,34 @@ function suurim(n){
 function lower(n){
     return Math.min.apply(null, n)
 }
-function even(n){
-    return n.filter(number => number % 2 == 0);
+const p=[]
+function even(n1){
+    
+    for(let i = 0; i < 5; i++)
+    {
+     if (n1[i] % 2 == 0){
+        p.push("true")
+     }   
+     else{
+         p.push("false")
+     }
+    }
+    var s = 0
+    for(let i=0;i<5;i++){
+        if(p[i] == "true"){s++}
+    }
+    return s
 }
-const j = [ 55, 90, 89, 52, 36]
-console.log(suvaline())
+
+const j = []
+for(let i = 0; i < 5; i++){
+    j.push(suvaline())
+}
+console.log(j)
 console.log(suurim(j))
 console.log(lower(j))
 console.log(even(j))
+
+
 
 
