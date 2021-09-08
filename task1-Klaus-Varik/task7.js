bd764f056fb9bf6ed9b150f25214bc77692c48bd
Klaus Varik <klaus.varik@vikk.ee>
2021-09-08 14:10:24 +0300

    task7 done

--- task1-Klaus-Varik/task7.js
+++ task1-Klaus-Varik/task7.js
@@ -1,17 +1,22 @@
 function getRandom(min, max){
     return Math.random(0) * (max - min) + min;
 }
-
 function suvaline(){
     return Math.round(getRandom(1, 100))
 }
 function suurim(n){
     return Math.max.apply(null, n)
 }
-console.log(suvaline())
-
-
+function lower(n){
+    return Math.min.apply(null, n)
+}
+function even(n){
+    return n.filter(number => number % 2 == 0);
+}
 const j = [ 55, 90, 89, 52, 36]
-
+console.log(suvaline())
 console.log(suurim(j))
+console.log(lower(j))
+console.log(even(j))
+
 
