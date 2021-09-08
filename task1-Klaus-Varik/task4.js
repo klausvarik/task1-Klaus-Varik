c67c1ed655e8ac880eb581f675e45ff101fdb9fb
Klaus Varik <klaus.varik@vikk.ee>
2021-09-08 13:17:12 +0300

    task4 done

---
+++ task1-Klaus-Varik/task4.js
@@ -0,0 +1,6 @@
+//Array
+const colors = ['Red','Green','White','Black'];
+
+console.log('"' + colors[0] + ',' + colors[1] + ',' + colors[2] + ',' + colors[3] + '."');
+console.log('"' + colors[0] + ',' + colors[1] + ',' + colors[2] + ',' + colors[3] + ',"');
+console.log('"' + colors[0] + '+' + colors[1] + '+' + colors[2] + '+' + colors[3] + '+"');
