4e59dc456a41ac8fd65ca33e0ab9390ec649e18f
Klaus Varik <klaus.varik@vikk.ee>
2021-10-14 09:03:47 +0300

    Fixed Task1

--- task1.js
+++ task1.js
@@ -1,13 +1,12 @@
 //Muutujad
-a = 10;
+let a = 10;
 const b = 5;
 // Liidab a ja b
 const c = a+b;
 // logib c väärtuse
 console.log(c);
 // a muutuja väärtus 5-ks
-a = 5;
 // liidab a ja b
-const d = a+b; 
+const d = a.toString()+b; 
 // logib d väärtuse
 console.log(d);
