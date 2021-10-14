a483f1da0d72aae206537871bffd8715c02c570d
Klaus Varik <klaus.varik@vikk.ee>
2021-10-14 09:24:38 +0300

    task3 ja task5 muudatus

--- task5.js
+++ task5.js
@@ -1,17 +1,17 @@
 //kas sisend on tühi
 function checkString(p1){
-    if(p1 === undefined){
+    if(!p1){
         return true;
     }
     return false;
 }
 //Suuretähe funktsioon
-function suurtaht(p1){
+function capitalletter(p1){
     return p1.charAt(0).toUpperCase() + p1.slice(1);
 }
 console.log(checkString(''));
 console.log(checkString('tere'));
 console.log(checkString('minu nimi on John'));
-console.log(suurtaht(''));
-console.log(suurtaht('tere'));
-console.log(suurtaht('minu nimi on John'));
+console.log(capitalletter(''));
+console.log(capitalletter('tere'));
+console.log(capitalletter('minu nimi on John'));
